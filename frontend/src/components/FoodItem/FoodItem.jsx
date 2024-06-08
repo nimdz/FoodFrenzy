9a1afd16f6fc6373d47b4e4c74f6df433aa2d2c8
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-06-08 21:44:16 +0530

    Update FoodItem.jsx

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -4,14 +4,14 @@ import { assets } from '../../assets/assets';
 import { StoreContext } from '../../context/StoreContext';
 
 const FoodItem = ({ id, name, price, description, image }) => {
-    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+    const { cartItems, addToCart, removeFromCart,url } = useContext(StoreContext);
     const itemCount = cartItems[id] || 0;
 
     return (
         <div>
             <div className="food-item">
                 <div className="food-item-img-container">
-                    <img className="food-item-image" src={image} alt="" />
+                    <img className="food-item-image" src={url+"/images/"+image} alt="" />
                     {!itemCount ? (
                         <img
                             className='add'
